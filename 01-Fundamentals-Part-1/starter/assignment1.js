4fd62b141f2d54f967fffa86c79747302dbb03be
BAGCHI <abhishekabk@gmail.com>
2024-06-13 19:46:41 +0530

    Variable Types and Naming(script.js) and Assignment on Variable Declaration

---
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent = "Asia";
+let population = "1.4 Billion";
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,11 @@
 let js = "amazing";
 
 if (js === "amazing") {
-    alert("JavaScript is FUN!");
+    // alert("JavaScript is FUN!");
+    console.log("JavaScript is FUN!");
 }
 
 console.log(40 + 8 + 23 - 10);
+
+let firstName = "Abhishek"; // Camel Case for naming Variables
+
