e9349027c7f54afa9446343c58698f630b8cda0b
BAGCHI <abhishekabk@gmail.com>
2024-07-05 17:42:23 +0530

    Using Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Using Strict Mode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true; // Using Strict Mode will throw an Error: hasDriverLicence is not defined
+if(hasDriversLicense) console.log("I can Drive");
+
+
