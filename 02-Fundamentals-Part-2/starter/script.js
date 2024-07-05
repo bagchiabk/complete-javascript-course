3fa4e82e982ef58f081e8f4bc41f3a4df1ecca3c
BAGCHI <abhishekabk@gmail.com>
2024-07-05 21:02:10 +0530

    Functions Introduction

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -5,7 +5,28 @@
 let hasDriversLicense = false;
 const passTest = true;
 
-if(passTest) hasDriverLicense = true; // Using Strict Mode will throw an Error: hasDriverLicence is not defined
+if(passTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log("I can Drive");
 
 
+// FUNCTIONS
+
+function logger() {
+    console.log("My name is Abhishek");
+}
+
+logger();
+
+
+function fruitProcessor(apple, oranges) {
+    console.log(apple, oranges);
+    const juice = `Juice with ${apple} and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(5, 6));
